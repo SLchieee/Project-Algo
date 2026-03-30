d576864132e18cdc1ad3fe7952c16371993e8946
SLchieee <richie.kenrick@gmail.com>
2026-03-30 14:13:31 +0700

    bubble sort

---
+++ 32.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a[5];
+    for(int i=0;i<5;i++)
+        scanf("%d",&a[i]);
+    for(int i=0;i<4;i++)
+        for(int j=0;j<4-i;j++)
+            if(a[j]>a[j+1]){
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+    for(int i=0;i<5;i++)
+        printf("%d ",a[i]);
+}
